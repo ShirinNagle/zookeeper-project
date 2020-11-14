0eeb5d7a07614e6120fec0d50aef6f1fe1bbccd7
Shirin <g00363458@gmit.ie>
2020-11-14 13:08:08 +0000

    Testing some methods

--- cluster-healer/src/main/java/ClusterHealer.java
+++ cluster-healer/src/main/java/ClusterHealer.java
@@ -1,51 +1,47 @@
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 public class ClusterHealer implements Watcher{
 
     // Path to the worker jar
     private final String pathToProgram;
-    // The number of worker instances we need to maintain at all times
-    private final int numberOfWorkers;
+    private final int numberOfWorkers;// The number of worker instances needs maintain at all times
     //Code added by me
     private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
     private static final int SESSION_TIMEOUT = 3000;
-    public static final String CLUSTER_NAMESPACE = "/worker";
-    private String currentZnodeName;
-
+    public static final String PARENT_NAMESPACE = "/worker";
+    private int runningWorkers;
+    String parentName = "";
     private ZooKeeper zooKeeper;
-    private boolean isParent = false;//boolean to check if the znode is a parent
 
     public ClusterHealer(int numberOfWorkers, String pathToProgram) {
         this.numberOfWorkers = numberOfWorkers;
         this.pathToProgram = pathToProgram;
     }
-
     /**
      * Check if the `/workers` parent znode exists, and create it if it doesn't. Decide for yourself what type of znode
      * it should be (e.g.persistent, ephemeral etc.). Check if workers need to be launched.
      */
     public void initialiseCluster() throws KeeperException, InterruptedException {
 
-        // **************
-        // YOUR CODE HERE
-        // **************
-        //Persistant node as this node is the parent node - don't want this node to die.
-        String znodePrefix = CLUSTER_NAMESPACE + "/w_";
-        String znodeFullPath = zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
-        System.out.println("znode name " + znodeFullPath);
-        this.currentZnodeName = znodeFullPath.replace(CLUSTER_NAMESPACE + "/", "");
-        System.out.println("znode name " + currentZnodeName);
+        //Persistant mode as this node is the parent node - don't want this node to die.
+        //need to check if parent exists - if it doesn't need to create a parent
+        //persistent parent created
+            parentName = zooKeeper.create(PARENT_NAMESPACE, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            System.out.println("Parent name is: " + parentName);
+
+
     }
     /**
      * Instantiates a Zookeeper client, creating a connection to the Zookeeper server.
      */
     public void connectToZookeeper() throws IOException {
-        // **************
-        // YOUR CODE HERE
-        // **************
+
         this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
     }
 
@@ -53,19 +49,17 @@ public class ClusterHealer implements Watcher{
      * Keeps the application running waiting for Zookeeper events.
      */
     public void run() throws InterruptedException {
-        // **************
-        // YOUR CODE HERE
-        // **************
-        zooKeeper.wait();
+
+        synchronized (zooKeeper)
+        {
+            zooKeeper.wait();
+        }
     }
 
     /**
      * Closes the Zookeeper client connection.
      */
     public void close() throws InterruptedException {
-        // **************
-        // YOUR CODE HERE
-        // **************
         zooKeeper.close();
     }
 
@@ -76,19 +70,39 @@ public class ClusterHealer implements Watcher{
      * @param event A Zookeeper event
      */
     public void process(WatchedEvent event) {
-        // **************
-        // YOUR CODE HERE
-        // **************
+
+        switch (event.getType()) {
+            case None:
+                if (event.getState() == Event.KeeperState.SyncConnected) {
+                    System.out.println("Successfully connected to Zookeeper");
+                } else {
+                    synchronized (zooKeeper) {
+                        System.out.println("Disconnected from Zookeeper event");
+                        zooKeeper.notifyAll();
+                    }
+                }
+                break;
+            case NodeDeleted:
+                //if a node is deleted need to notify checking workers - or checking workers needs to notify watchedEvent!!
+        }
+
+
     }
 
     /**
      * Checks how many workers are currently running.
      * If less than the required number, then start a new worker.
      */
-    public void checkRunningWorkers() {
-        // **************
-        // YOUR CODE HERE
-        // **************
+    public void checkRunningWorkers() throws KeeperException, InterruptedException {
+
+        List<String> workers = zooKeeper.getChildren(parentName, this);
+
+        System.out.println(workers);
+
+        if(workers.size() < numberOfWorkers){
+
+        }
+
     }
 
     /**
